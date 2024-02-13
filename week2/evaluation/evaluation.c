bdf1f59276e36bf2b6fec064fecfa0c0ba6b9190
hansol <hasnol@paradive.io>
2024-02-13 16:47:54 +0900

    c express evaluation

---
+++ week2/evaluation/evaluation.c
@@ -0,0 +1,23 @@
+/*
+    Expression Evaluation
+    Written by:  Hansol L
+    Date:  Feb/13/24
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int a = 5, b = 7, c = 0, d = 0; //initialize 
+    c = a - b;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+    c = b - a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+    c = a / b; d = b / a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+    c = a % b; d = b % a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+    c = -a - b; d = -b - a;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+    c = ++a + b++; d += 5;
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+}
