86cc0b19be7c67b0162b432155f715422ad14e4a
hansol <hasnol@paradive.io>
2024-02-13 15:24:37 +0900

    marathon in km

---
+++ marathon/marathon.c
@@ -0,0 +1,16 @@
+/*
+    Marathon distance in kilometers
+    Written by:  Hansol L
+    Date:  2/13/24
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int miles = 26, yards = 385;
+    double kilometers;
+
+    kilometers = 1.609 * (miles + yards / 1760.0);
+    printf("\nA marathon is %lf kilometers.\n", kilometers);
+    return 0;
+}
