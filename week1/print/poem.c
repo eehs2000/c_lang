4efe9f8051e4e906ea7008bcaa0b7ed8e43fc646
hansol <hasnol@paradive.io>
2024-02-13 16:19:14 +0900

    restructure

---
+++ week1/print/poem.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(void)
+{   
+    printf("The Road Not Taken by Robert Frost\n\n");
+    printf("Two roads diverged in a yellow wood,\n");
+    printf("And sorry I could not travel both\n");
+    printf("And be one traveler, long I stood\n");
+    printf("And looked down one as far as I could\n");
+    printf("To where it bent in the undergrowth;\n\n");
+    
+    printf("Then took the other, as just as fair,\n");
+    printf("And having perhaps the better claim,\n");
+    printf("Because it was grassy and wanted wear;\n");
+    printf("Though as for that the passing there\n");
+    printf("Had worn them really about the same,\n\n");
+    
+    printf("And both that morning equally lay\n");
+    printf("In leaves no step had trodden black.\n");
+    printf("Oh, I kept the first for another day!\n");
+    printf("Yet knowing how way leads on to way,\n");
+    printf("I doubted if I should ever come back.\n\n");
+    
+    printf("I shall be telling this with a sigh\n");
+    printf("Somewhere ages and ages hence:\n");
+    printf("Two roads diverged in a wood, and I—\n");
+    printf("I took the one less traveled by,\n");
+    printf("And that has made all the difference.\n");
+    return 0;
+}    
