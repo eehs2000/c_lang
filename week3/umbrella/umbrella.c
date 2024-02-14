631060db85bbcbfab8dd814529280f979da22226
hansol <hasnol@paradive.io>
2024-02-14 11:56:56 +0900

    umbrella

---
+++ week3/umbrella/umbrella.c
@@ -0,0 +1,20 @@
+/*
+    Do I need an umbrella?
+    Written by:  Hansol L
+    Date:  Feb/14/24
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int weather;
+    printf("What is the weather today? (0 for sunny, 1 for rainy): ");
+    scanf("%d", &weather);
+    if(weather == 0){
+        printf("You don't need an umbrella today\n");
+    }
+    else if(weather == 1){
+        printf("You need an umbrella today\n");
+    }
+    return 0;
+}
